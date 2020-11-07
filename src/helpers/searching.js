9cb028205a3373726c58497f2ebd1467a639c2a2
prmstrgt <pramestiregitaa@gmail.com>
2020-11-07 10:00:19 +0700

    helpers for sorting and searching

---
+++ src/helpers/searching.js
@@ -0,0 +1,35 @@
+module.exports = {
+  users: (search) => {
+    if (typeof search === 'object') {
+      const searchKey = Object.keys(search)[0]
+      const searchValue = Object.values(search)[0]
+      return { searchKey, searchValue }
+    } else {
+      const searchKey = 'fullName'
+      const searchValue = search || ''
+      return { searchKey, searchValue }
+    }
+  }
+  // email: (search) => {
+  //   if (typeof search === 'object') {
+  //     const searchKey = Object.keys(search)[0]
+  //     const searchValue = Object.values(search)[0]
+  //     return { searchKey, searchValue }
+  //   } else {
+  //     const searchKey = 'email'
+  //     const searchValue = search || ''
+  //     return { searchKey, searchValue }
+  //   }
+  // },
+  // transaction: (search) => {
+  //   if (typeof search === 'object') {
+  //     const searchKey = Object.keys(search)[0]
+  //     const searchValue = Object.values(search)[0]
+  //     return { searchKey, searchValue }
+  //   } else {
+  //     const searchKey = 'product_name'
+  //     const searchValue = search || ''
+  //     return { searchKey, searchValue }
+  //   }
+  // }
+}
---
+++ src/helpers/sorting.js
@@ -0,0 +1,35 @@
+module.exports = {
+  roles: (sort) => {
+    if (typeof sort === 'object') {
+      const sortKey = Object.keys(sort)[0]
+      const sortBy = Object.values(sort)[0] || 'ASC'
+      return { sortKey, sortBy }
+    } else {
+      const sortKey = 'id'
+      const sortBy = sort || 'ASC'
+      return { sortKey, sortBy }
+    }
+  }
+  // email: (sort) => {
+  //   if (typeof sort === 'object') {
+  //     const sortKey = Object.keys(sort)[0]
+  //     const sortBy = Object.values(sort)[0] || 'ASC'
+  //     return { sortKey, sortBy }
+  //   } else {
+  //     const sortKey = 'email'
+  //     const sortBy = sort || 'ASC'
+  //     return { sortKey, sortBy }
+  //   }
+  // },
+  // transaction: (sort) => {
+  //   if (typeof sort === 'object') {
+  //     const sortKey = Object.keys(sort)[0]
+  //     const sortBy = Object.values(sort)[0] || 'ASC'
+  //     return { sortKey, sortBy }
+  //   } else {
+  //     const sortKey = 'created_at'
+  //     const sortBy = sort || 'ASC'
+  //     return { sortKey, sortBy }
+  //   }
+  // }
+}
