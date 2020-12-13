b263fa10bfcec107efb47706e4eb7caf78fc3616
prmstrgt <pramestiregitaa@gmail.com>
2020-12-14 05:26:10 +0700

    bug fixes

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -5,49 +5,86 @@ const search = require('../helpers/searching')
 const sort = require('../helpers/sorting')
 const paging = require('../helpers/pagination')
 const { Op } = require('sequelize')
+const upload = require('../helpers/upload').single('picture')
+const multer = require('multer')
+const fs = require('fs')
 
 module.exports = {
   createPost: async (req, res) => {
-    try {
-      const { id: userId } = req.user
-      const schema = Joi.object({
-        title: Joi.string().messages({
-          'string.empty': 'Title can\'t be empty'
-        }),
-        news: Joi.string().messages({
-          'string.empty': 'Post can\'t be empty'
+    upload(req, res, async (err) => {
+      let picture = {}
+      try {
+        if (err instanceof multer.MulterError) {
+          return response(res, err.message, {}, 500, false)
+        } else if (err) {
+          return response(res, err.message, {}, 500, false)
+        }
+        const { id: userId } = req.user
+        const schema = Joi.object({
+          title: Joi.string().messages({
+            'string.empty': 'Title can\'t be empty'
+          }),
+          news: Joi.string().messages({
+            'string.empty': 'Post can\'t be empty'
+          })
         })
-      })
-      const find = await Users.findByPk(userId)
-      if (find) {
-        const { value, error } = schema.validate(req.body)
-        if (error) {
-          return response(res, error.message, {}, 400, false)
-        }
-        const { title, news } = value
-        const data = { userId, title, news }
-        const results = await Post.create(data)
-        if (results) {
-          return response(res, 'Create post successfully', { data: results }, 201)
+        const find = await Users.findByPk(userId)
+        if (find) {
+          const { value, error } = schema.validate(req.body)
+          if (error) {
+            return response(res, error.message, {}, 400, false)
+          }
+          if (req.file === undefined) {
+            return response(res, 'Please select an image', {}, 400, false)
+          } else {
+            picture = req.file
+          }
+          const { title, news } = value
+          const data = { userId, title, news }
+          const results = await Post.create(data)
+          if (results) {
+            const image = {
+              userId,
+              postId: results.dataValues.id,
+              image: 'upload/' + picture.filename
+            }
+            console.log(results.dataValues.id)
+            const upload = await PostImage.create(image)
+            if (upload) {
+              return response(res, 'Create post successfully', { data: [results, upload] }, 201)
+            } else {
+              picture && fs.unlink(picture.path, (err) => {
+                if (err) {
+                  return response(res, err.message, {}, 400, false)
+                }
+              })
+              return response(res, 'Failed to create', {}, 400, false)
+            }
+          } else {
+            return response(res, 'Failed to create', {}, 400, false)
+          }
         } else {
-          return response(res, 'Failed to create', {}, 400, false)
+          return response(res, 'User not found', {}, 404, false)
         }
-      } else {
-        return response(res, 'User not found', {}, 404, false)
+      } catch (err) {
+        picture && fs.unlink(picture.path, (err) => {
+          if (err) {
+            return response(res, err.message, {}, 400, false)
+          }
+        })
+        if (err.message.includes('\n')) {
+          return response(res, 'All fields must be filled', {}, 400, false)
+        } else if (err.message.includes('notNull')) {
+          const msg = err.message.slice(19)
+          return response(res, msg, {}, 400, false)
+        } else if (err.errors) {
+          const msg = err.errors[0].message
+          return response(res, msg, {}, 400, false)
+        } else {
+          return response(res, err.message, {}, 400, false)
+        }
       }
-    } catch (err) {
-      if (err.message.includes('\n')) {
-        return response(res, 'All fields must be filled', {}, 400, false)
-      } else if (err.message.includes('notNull')) {
-        const msg = err.message.slice(19)
-        return response(res, msg, {}, 400, false)
-      } else if (err.errors) {
-        const msg = err.errors[0].message
-        return response(res, msg, {}, 400, false)
-      } else {
-        return response(res, err.message, {}, 400, false)
-      }
-    }
+    })
   },
   getPosts: async (req, res) => {
     try {
@@ -81,7 +118,10 @@ module.exports = {
             as: 'picture',
             attributes: ['image']
           }
-        ]
+        ],
+        attributes: {
+          exclude: ['news']
+        }
       })
       if (results) {
         return response(res, 'List of News', { data: results, pageInfo })
@@ -150,7 +190,10 @@ module.exports = {
               as: 'author',
               attributes: ['fullName']
             }
-          ]
+          ],
+          attributes: {
+            exclude: ['news']
+          }
         })
         if (results.length) {
           return response(res, 'List of News', { user, data: results, pageInfo })
@@ -227,7 +270,10 @@ module.exports = {
             as: 'picture',
             attributes: ['image']
           }
-        ]
+        ],
+        attributes: {
+          exclude: ['news']
+        }
       })
       if (results) {
         return response(res, 'List of News', { data: results, pageInfo })
