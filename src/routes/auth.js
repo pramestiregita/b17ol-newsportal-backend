d9c98c3bce0cc1e4047dff14b0c86ff1c5ca6241
prmstrgt <pramestiregitaa@gmail.com>
2020-12-14 05:59:00 +0700

    edit

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,15 @@
 const route = require('express').Router()
 const authController = require('../controllers/auth')
 const userController = require('../controllers/users')
+const rolesController = require('../controllers/roles')
 
 // login
 route.post('/login/:role', authController)
 
 // register user
 route.post('/signup/:role', userController.registerUser)
+
+route.post('/role', rolesController.createRole)
+route.post('/user/:role', userController.createUser)
 
 module.exports = route
